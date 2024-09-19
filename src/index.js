25819f683ff5d7f484f3d28f168f1cbcba09f631
SOURABHPATIL777 <83913206+SOURABHPATIL777@users.noreply.github.com>
2024-09-19 11:41:14 +0530

    project setup

---
+++ src/index.js
@@ -0,0 +1,50 @@
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+import mongoose from 'mongoose';
+import { DB_NAME } from './constants';
+
+import express from 'express';
+const app = express();
+
+( async ()=> {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+        app.on("Error", (error)=>{
+            console.log('Error', error);
+            throw error
+        });
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on server ${process.env.PORT}`);
+        })
+
+
+    } catch (error){
+        console.error('Error: ', error);
+        throw error
+    }
+} )();
+
+*/
