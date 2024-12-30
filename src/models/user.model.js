7c32bf18a98eb138fe2e43802b7f78c574b21111
SOURABHPATIL777 <07sourabhpatil@gmail.com>
2024-12-30 23:38:01 +0530

    creating user and video models with jwt token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
     {
@@ -17,7 +19,7 @@ const userSchema = new Schema(
             lowercase: true,
             trim: true
         },
-        fullname: {
+        fullName: {
             type: String,
             required: true,
             trim: true
@@ -48,5 +50,44 @@ const userSchema = new Schema(
         timestamps: true
     }
 );
+
+userSchema.pre("save", async function (next) {
+    if(this.isModified("password")){
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+
+    next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = async function (){
+    return jwt.sign(
+        {
+            _id: this._id,
+            userName: this.userName,
+            fullName: this.fullName,
+            email: this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    );
+}
+
+userSchema.methods.generateRefreshToken = async function(){
+    return jwt.JsonWebTokenError(
+        {
+            _id: this._id,
+        },
+        process.env.ACCESS_REFRESH_TOKEN,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    );
+}
 
 export const User = mongoose.model('User', userSchema);
